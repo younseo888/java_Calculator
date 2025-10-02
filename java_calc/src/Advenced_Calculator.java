4f9de66c7dd7bb18aea3a01707292e0c99183895
younseo <part44480@gmail.com>
2025-10-02 11:32:42 +0900

    adven_clac 주석 추가

---
+++ java_calc/src/Advenced_Calculator.java
@@ -0,0 +1,16 @@
+public class Advenced_Calculator extends calculator{
+
+
+
+    @Override
+    public int multiply(){
+
+        result = num1 * num2;
+        return result;
+    }
+    @Override
+    public int divide(){
+        result = num1 / num2;
+        return result;
+    }
+}
--- java_calc/src/Main.java
+++ java_calc/src/Main.java
@@ -6,5 +6,10 @@ public class Main {
 
         calc.add();
         calc.displayresult("+");
+
+        Advenced_Calculator calc_mul = new Advenced_Calculator();
+        calc_mul.setNumbers(10,2);
+        calc_mul.multiply();
+        calc_mul.displayresult("*");
     }
 }
